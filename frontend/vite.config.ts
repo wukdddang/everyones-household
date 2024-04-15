c750234f83ebe4ab3d2760f80067bedb61001039
wukdddang <dnr8874@naver.com>
2024-04-15 18:17:43 +0900

    Chore: 빌드 관련 코드 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    eslintPlugin({
+      emitWarning: true, // 경고로만 처리
+      emitError: false, // 에러를 발생시키지 않음
+      failOnWarning: false, // 경고 상태에서 빌드 실패하지 않음
+      failOnError: false, // 에러 상태에서 빌드 실패하지 않음
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
